13bf38bf7d953445a25e45bf12479b102d21dde9
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 16:10:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ def line(customers)
   return "The line is currently empty." if customers.length == 0
   line_string = "The line is currently:"
   customers.each_with_index { |customer, index|
-    line_string + " #{index}. #{customer}"
+    line_string + " #{index + 1}. #{customer}"
   }
   line_string
+end
+
+def take_a_number(line, name)
+  "Welcome, #{name}. You are number #{line.index(name) + 1}"
 end
