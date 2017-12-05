3c5eb799b7b32009152307cad49d25b3e68ae256
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 15:35:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(customers)
   return "The line is currently empty." if customers.length == 0
   line_string = "The line is currently:"
   customers.each_with_index { |customer, index|
-    line_string + " #{index.} #{customer}"
+    line_string + " #{index}. #{customer}"
   }
 end
